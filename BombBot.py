6e3c7b01b9ecfa3c59a2f2d268da526bbfd35686
Shiina001 <88496014+Shiina001@users.noreply.github.com>
2021-08-06 13:38:42 +0200

    Update BombBot.py

--- BombBot.py
+++ BombBot.py
@@ -22,13 +22,13 @@ code_box = driver.find_element_by_css_selector(".joinRoom input[type=text]")
 code_box.send_keys(code)
 code_box.send_keys(Keys.RETURN)
 
-time.sleep(5)
+time.sleep(3)
 
 name_box = driver.find_element_by_css_selector(".setNickname.page .nickname")
 name_box.send_keys(name)
 name_box.send_keys(Keys.RETURN)
 
-time.sleep(5)
+time.sleep(3)
 
 iframe = driver.find_element_by_css_selector("iframe")
 driver.switch_to.frame(iframe)
@@ -40,15 +40,13 @@ while True:
     if(button.is_displayed()):
         time.sleep(1)
         button.click()
-        time.sleep(2)
-    
+        time.sleep(1)
     
     player = driver.find_element_by_class_name("player").text
     input_box = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div[2]/div[2]/form/input")
-    
-        
+      
     if(player == "" and input_box.is_displayed()):
-        time.sleep(1)
+        time.sleep(0.25)
         syl = driver.find_element_by_class_name("syllable").text
         syl = syl.lower()
         print("It's your turn with syllable: " + syl )
@@ -64,16 +62,24 @@ while True:
             x = x + 1
 
         print("Words found: ")
-        print(words)
-
         y = len(words)
+        print(y)
+
+      
         while input_box.is_displayed():
-            input_box.send_keys(words[random.randint(0, y)])
+            q = 0
+            final = words[random.randint(0, y)]
+            z = len(final)
+            time.sleep(random.uniform(0.3, 1))
+            for i in range(z):
+                input_box.send_keys(final[q])
+                q = q + 1
+                time.sleep(random.uniform(0.01, 0.25))
+
             input_box.send_keys(Keys.RETURN)
             y = y + 1
-            time.sleep(0.25)
+            time.sleep(0.5)
 
         words.clear()
-
-        f.close()        
-        time.sleep(2)
+        
+        f.close()
